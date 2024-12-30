15ccd2d770b4ff4b4caa72d3e6c364afe9dcff38
Louis Sin <4rc@arcenciel.dev>
2024-12-30 13:13:04 +0800

    2nd stage

--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,10 +3,11 @@ import "../css/TechStack.css";
 function TechStack() {
   const techStack = [
     { name: "HTML", icon: "icons/Techstack/HTML5.svg" },
-    { name: "CSS", icon: "images/placeholder.png" },
-    { name: "JavaScript", icon: "images/placeholder.png" },
-    { name: "mySQL", icon: "images/placeholder.png" },
-    { name: "vueJS", icon: "images/placeholder.png" },
+    { name: "CSS", icon: "icons/Techstack/CSS3.svg" },
+    { name: "Javascript", icon: "icons/Techstack/JavaScript.svg" },
+    { name: "NodeJS", icon: "icons/Techstack/NodeJS.svg" },
+    { name: "Vue.js", icon: "icons/Techstack/VueJS.svg" },
+    { name: "mySQL", icon: "icons/Techstack/mySQL.svg" },
   ];
 
   return (
